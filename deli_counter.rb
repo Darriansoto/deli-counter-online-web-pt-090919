dee5f292b86e7a5c95f3f06701da36e48ca7c874
Darrian Soto <ddog350@gmail.com>
2019-09-24 22:48:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,41 @@
-# Write your code here.
+def line(array)
+  if array.size >= 1
+    counter = 1 
+    new_array = []
+    array.each do |name|
+      new_array.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  if array.empty? == true 
+    new_array = []
+    counter = 1
+    array.map do |name|
+      new_array.push("#{array}. #{name}")
+      counter += 1
+    end
+    puts "Welcome, #{name}. You are number #{counter} in line."
+  elsif array.empty? == false
+    new_array = []
+    counter = 1 
+    array.map do |name|
+      
+    end
+  end
+  
+  
+end
+
+def now_serving(array)
+  if array.empty? == true 
+    
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end

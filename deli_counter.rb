e06fc532f188b5550556c81351aa020a1344bcb1
Darrian Soto <ddog350@gmail.com>
2019-09-24 23:24:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(array, name)
     new_array = []
     counter = 1
     array.map do |name|
-      new_array.push("#{name}")
+      new_array.push("#{array}. #{name}")
       counter += 1
     end
     puts "Welcome, #{name}. You are number #{counter} in line."
